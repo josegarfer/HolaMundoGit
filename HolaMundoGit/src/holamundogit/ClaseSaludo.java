7a598ad2b59ef4ec53cc108bec0526fef824a51a
Pepe <>
2020-10-23 09:12:12 +0200

    Primer contacto con GitHub de una clase Java

---
+++ HolaMundoGit/src/holamundogit/ClaseSaludo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundogit;
+
+/**
+ *
+ * @author Pepe
+ */
+public class ClaseSaludo {
+    private String nombre;
+    
+    public ClaseSaludo(String nombre){
+        this.nombre=nombre;
+        System.out.println("Hola "+nombre+" desde GitHub");
+    }
+    
+}
